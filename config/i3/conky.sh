2ef023b444aeef7d68c93e856b4185d32091a0fa
ne0-n00b <35328706+ne0-n00b@users.noreply.github.com>
2018-01-23 15:02:12 +0000

    Add files via upload

---
+++ config/i3/conky.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Send the header so that i3bar knows we want to use JSON:
+echo '{"version":1}'
+
+# Begin the endless array.
+echo '['
+
+# We send an empty first array of blocks to make the loop simpler:
+echo '[],'
+
+# Now send blocks with information forever:
+#conky -c ~/.config/i3/conky
+conky -c $HOME/.config/i3/conky | iconv -t UTF-8
